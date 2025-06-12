dc227dcc0cacaf97ea1d25a53dab7a08913e6012
David Funaro <dfunaro@amazon.com>
2025-06-12 18:32:11 +0100

    package renamed to cloudwatch-mcp-server

--- src/cloudwatch-mcp-server/awslabs/cloudwatch_metrics_mcp_server/__init__.py
+++ src/cloudwatch-mcp-server/awslabs/cloudwatch_metrics_mcp_server/__init__.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""awslabs.cloudwatch-metrics-mcp-server"""
+"""awslabs.cloudwatch-mcp-server"""
 
 MCP_SERVER_VERSION = '0.0.1'
 
